dc7cc42a19e5523764c40734fbd946be2ba4e309
Vinicius Meira <viniciusmeirabr@hotmail.com>
2022-06-24 21:23:15 -0300

    Trabalhando com rotas filhas e templates diferentes

--- src/app/clientes/clientes-form/clientes-form.component.ts
+++ src/app/clientes/clientes-form/clientes-form.component.ts
@@ -66,7 +66,7 @@ export class ClientesFormComponent implements OnInit {
   }
 
   voltarParaListagem() {
-    this.router.navigate(['clientes-lista']);
+    this.router.navigate(['clientes/lista']);
   }
 
 }
